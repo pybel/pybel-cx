034ec48e4a398df540faef6bf3bbba89d3d56373
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-06-05 22:28:42 +0200

    Add bump version configuration

--- src/pybel_cx/constants.py
+++ src/pybel_cx/constants.py
@@ -7,6 +7,9 @@ __all__ = [
     'NDEX_PASSWORD',
 ]
 
+#: The version of PyBEL-CX
+VERSION = '0.1.0-dev'
+
 #: The name of the environment variable to search or the NDEx username
 NDEX_USERNAME = 'NDEX_USERNAME'
 

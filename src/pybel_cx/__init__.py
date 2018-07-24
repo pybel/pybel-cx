62590cffe242222c55c4ceb5a02e6476b0021c43
Charles Tapley Hoyt <cthoyt@wlan-224.scai.fraunhofer.de>
2018-07-24 14:35:50 +0200

    Bump version: 0.1.1-dev → 0.1.1

--- src/pybel_cx/__init__.py
+++ src/pybel_cx/__init__.py
@@ -66,7 +66,7 @@ __all__ = [
     'to_ndex',
 ]
 
-__version__ = '0.1.1-dev'
+__version__ = '0.1.1'
 
 __title__ = 'pybel_cx'
 __description__ = 'A PyBEL extension for interconversion with CX'
--- src/pybel_cx/constants.py
+++ src/pybel_cx/constants.py
@@ -8,7 +8,7 @@ __all__ = [
 ]
 
 #: The version of PyBEL-CX
-VERSION = '0.1.1-dev'
+VERSION = '0.1.1'
 
 #: The name of the environment variable to search or the NDEx username
 NDEX_USERNAME = 'NDEX_USERNAME'
